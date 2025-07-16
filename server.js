6f5206a3ab87a0baba1964d871ad57f82c5aa21e
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 14:08:47 +0300

    modify: updated server.js (middleware, routes, or configs)

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { connectDB } from "./ConnectDB/DB.js";
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Connected on 3000 successfully");
+  connectDB();
 });
