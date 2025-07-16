3a042110afeba1ec0ec2dc318e2f0b1f0225a75d
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 20:48:41 +0300

    add: added UpdateImageUser controller to handle profile image updates

---
+++ Controller/GetDataByuserId/UpdateImageUser.js
@@ -0,0 +1,38 @@
+import Joi from "joi";
+import { UserSchema } from "./getLinks.js";
+import DB from "../../ConnectDB/DB.js";
+
+const ImageSchema = Joi.object({
+  imageName: Joi.string().required(),
+});
+
+export const UpdateImageUser = async (req, res) => {
+  const { error: ErrorImage, value: ImageValue } = ImageSchema.validate(
+    req.body
+  );
+  const { error: ErrorUserId, value: UserIdValue } = UserSchema.validate(
+    req.params
+  );
+  try {
+    if (ErrorImage || ErrorUserId) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+    const Query = "UPDATE user SET image = ? WHERE id = ?";
+    const Value = [ImageValue.imageName, UserIdValue.userId];
+
+    await DB.promise().query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Updated Successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
