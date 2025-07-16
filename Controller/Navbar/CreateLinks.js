bcdb201d5ea916658db4b6651a62bede1ec90453
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 16:26:06 +0300

    add: added Navbar controller for handling navbar-related logic

---
+++ Controller/Navbar/CreateLinks.js
@@ -0,0 +1,29 @@
+import Joi from "joi";
+import DB from "../../ConnectDB/DB.js";
+import { defaultNavbarLinks } from "../../Constant/NavbarLinks.js";
+
+const NavbarSchema = Joi.object({
+  userId: Joi.number().required(),
+});
+
+export const CreateLinks = async (req, res) => {
+  try {
+    const { value, error } = NavbarSchema.validate(req.body);
+
+    if (error) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+    const Query = "INSERT INTO navbar (Links, user_id) VALUES (?, ?)";
+    const Value = [JSON.stringify(defaultNavbarLinks), value.userId];
+
+    await DB.promise().query(Query, Value);
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
