3043b837a8f5f7d67e5e0da8df5892e0ffe2e851
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 20:48:37 +0300

    modify: extended UserRouter with additional endpoints or refinements

--- Routers/User/UserRouter.js
+++ Routers/User/UserRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { CreateNewUser } from "../../Controller/User/CreateNewUser.js";
+import { GetUserId } from "../../Controller/User/GetUserByid.js";
 
 const UserRouter = express.Router();
 
 UserRouter.route("/").post(CreateNewUser);
+UserRouter.route("/:userId").get(GetUserId);
 
 export default UserRouter;
