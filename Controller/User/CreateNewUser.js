df646669ed0ed480e956329e2cc047383502fd40
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 16:25:51 +0300

    modify: updated CreateNewUser controller logic or validation

--- Controller/User/CreateNewUser.js
+++ Controller/User/CreateNewUser.js
@@ -1,14 +1,22 @@
 import DB from "../../ConnectDB/DB.js";
+import { CreateLinks } from "../Navbar/CreateLinks.js";
 
 export const CreateNewUser = async (req, res) => {
   try {
     const Query = "INSERT INTO user (Name) VALUES (?)";
     const Value = ["Mohamed Osama"];
 
-    await DB.promise().query(Query, Value);
+    const [resultCraete] = await DB.promise().query(Query, Value);
+
+    const UserQuery = "SELECT * FROM user WHERE id = ?";
+    const [valueUser] = [resultCraete.insertId];
+
+    const [resultGet] = await DB.promise().query(UserQuery, valueUser);
+
     return res.status(200).json({
       statusbar: "success",
       message: "Created Successfully",
+      user: resultGet,
     });
   } catch (err) {
     return res.status(500).json({
