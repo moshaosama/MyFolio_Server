9e134de254e4094b0bee74b498f39e43decc1abb
moshaosama <mohamedOSFekry@gmail.com>
2025-07-16 20:48:35 +0300

    modify: updated DataByUserIdRouter with new route handlers or logic

--- Routers/DataByUserId/DataByUserIdRouter.js
+++ Routers/DataByUserId/DataByUserIdRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { GetLinks } from "../../Controller/GetDataByuserId/getLinks.js";
 import { DeleteLink } from "../../Controller/GetDataByuserId/DeleteLink.js";
+import { UpdateImageUser } from "../../Controller/GetDataByuserId/UpdateImageUser.js";
 
 const DataByUserIdRouter = express.Router();
+const EditImageRouter = express.Router();
 
 DataByUserIdRouter.route("/:userId").get(GetLinks).delete(DeleteLink);
-
-export default DataByUserIdRouter;
+EditImageRouter.route("/:userId").put(UpdateImageUser);
+export { DataByUserIdRouter, EditImageRouter };
