a0d95892e8e264301833c20dda492ae850491615
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 20:33:09 +0300

    feat: add controller to handle name editing

---
+++ Controller/GetDataByuserId/EditName.js
@@ -0,0 +1,43 @@
+import Joi from "joi";
+import { UserSchema } from "./getLinks.js";
+import DB from "../../ConnectDB/DB.js";
+
+const userNameSchema = Joi.object({
+  Name: Joi.string().required().min(3),
+});
+export const EditName = async (req, res) => {
+  const { value: NameValue, error: NameError } = userNameSchema.validate(
+    req.body
+  );
+  const { value: userIdValue, error: userIdError } = UserSchema.validate(
+    req.params
+  );
+  try {
+    if (NameError || userIdError) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+
+    const Query = "UPDATE user SET Name = ? WHERE id = ?";
+    const Value = [NameValue.Name, userIdValue.userId];
+
+    await DB.promise().query(Query, Value);
+
+    const QueryGetUser = "SELECT * FROM user WHERE id = ?";
+    const ValueGetUser = [userIdValue.userId];
+
+    const [result] = await DB.promise().query(QueryGetUser, ValueGetUser);
+
+    return res.status(200).json({
+      statusbar: "success",
+      user: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
