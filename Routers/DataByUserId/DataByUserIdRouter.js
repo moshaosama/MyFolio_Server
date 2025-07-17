2705925989f8446147694b51c15036f0cf306fbe
moshaosama <mohamedOSFekry@gmail.com>
2025-07-18 01:40:32 +0300

    chore: update DataByUserIdRouter with new endpoints

--- Routers/DataByUserId/DataByUserIdRouter.js
+++ Routers/DataByUserId/DataByUserIdRouter.js
@@ -4,14 +4,23 @@ import { DeleteLink } from "../../Controller/GetDataByuserId/DeleteLink.js";
 import { UpdateImageUser } from "../../Controller/GetDataByuserId/UpdateImageUser.js";
 import { Edittags } from "../../Controller/GetDataByuserId/EditTags.js";
 import { EditName } from "../../Controller/GetDataByuserId/EditName.js";
+import { EditBio } from "../../Controller/GetDataByuserId/EditBio.js";
 
 const DataByUserIdRouter = express.Router();
 const EditImageRouter = express.Router();
 const EditTagsRouter = express.Router();
 const EditNameRouter = express.Router();
+const EditBioRouter = express.Router();
 
 DataByUserIdRouter.route("/:userId").get(GetLinks).delete(DeleteLink);
 EditImageRouter.route("/:userId").put(UpdateImageUser);
 EditTagsRouter.route("/:userId").put(Edittags);
 EditNameRouter.route("/:userId").put(EditName);
-export { DataByUserIdRouter, EditImageRouter, EditTagsRouter, EditNameRouter };
+EditBioRouter.route("/:userId").put(EditBio);
+export {
+  DataByUserIdRouter,
+  EditImageRouter,
+  EditTagsRouter,
+  EditNameRouter,
+  EditBioRouter,
+};
