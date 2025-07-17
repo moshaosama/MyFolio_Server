724df4eb93c3e09ebbbbe83a718a675e62c040ef
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 20:33:08 +0300

    chore: update routes for DataByUserId APIs

--- Routers/DataByUserId/DataByUserIdRouter.js
+++ Routers/DataByUserId/DataByUserIdRouter.js
@@ -3,12 +3,15 @@ import { GetLinks } from "../../Controller/GetDataByuserId/getLinks.js";
 import { DeleteLink } from "../../Controller/GetDataByuserId/DeleteLink.js";
 import { UpdateImageUser } from "../../Controller/GetDataByuserId/UpdateImageUser.js";
 import { Edittags } from "../../Controller/GetDataByuserId/EditTags.js";
+import { EditName } from "../../Controller/GetDataByuserId/EditName.js";
 
 const DataByUserIdRouter = express.Router();
 const EditImageRouter = express.Router();
 const EditTagsRouter = express.Router();
+const EditNameRouter = express.Router();
 
 DataByUserIdRouter.route("/:userId").get(GetLinks).delete(DeleteLink);
 EditImageRouter.route("/:userId").put(UpdateImageUser);
 EditTagsRouter.route("/:userId").put(Edittags);
-export { DataByUserIdRouter, EditImageRouter, EditTagsRouter };
+EditNameRouter.route("/:userId").put(EditName);
+export { DataByUserIdRouter, EditImageRouter, EditTagsRouter, EditNameRouter };
