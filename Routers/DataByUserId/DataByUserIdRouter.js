b6d5b9ac67be439ad950f1b10d758820bf9838c7
moshaosama <mohamedOSFekry@gmail.com>
2025-07-17 18:32:04 +0300

    refactor: update DataByUserIdRouter

--- Routers/DataByUserId/DataByUserIdRouter.js
+++ Routers/DataByUserId/DataByUserIdRouter.js
@@ -2,10 +2,13 @@ import express from "express";
 import { GetLinks } from "../../Controller/GetDataByuserId/getLinks.js";
 import { DeleteLink } from "../../Controller/GetDataByuserId/DeleteLink.js";
 import { UpdateImageUser } from "../../Controller/GetDataByuserId/UpdateImageUser.js";
+import { Edittags } from "../../Controller/GetDataByuserId/EditTags.js";
 
 const DataByUserIdRouter = express.Router();
 const EditImageRouter = express.Router();
+const EditTagsRouter = express.Router();
 
 DataByUserIdRouter.route("/:userId").get(GetLinks).delete(DeleteLink);
 EditImageRouter.route("/:userId").put(UpdateImageUser);
-export { DataByUserIdRouter, EditImageRouter };
+EditTagsRouter.route("/:userId").put(Edittags);
+export { DataByUserIdRouter, EditImageRouter, EditTagsRouter };
