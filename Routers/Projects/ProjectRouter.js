2fa5d1cc34aa2eca9d89726be12a34a1d71dcb52
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 23:39:54 +0300

    chore: update project router with new endpoints

--- Routers/Projects/ProjectRouter.js
+++ Routers/Projects/ProjectRouter.js
@@ -2,10 +2,11 @@ import express from "express";
 import { GetProjects } from "../../Controller/Projects/getProjects.js";
 import { CreateProject } from "../../Controller/Projects/CreateProject.js";
 import { DeleteProject } from "../../Controller/Projects/DeleteProject.js";
+import { EditProjects } from "../../Controller/Projects/EditProjects.js";
 
 const ProjectRouter = express.Router();
 
 ProjectRouter.route("/:userId").get(GetProjects).post(CreateProject);
-ProjectRouter.route("/:project_id").delete(DeleteProject);
+ProjectRouter.route("/:project_id").delete(DeleteProject).put(EditProjects);
 
 export default ProjectRouter;
