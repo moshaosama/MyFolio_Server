cf9642338c0f4ac3d9f118acc5543bf72890a24c
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 16:41:23 +0300

    fix: enhance CreateNewUser logic to handle new project-related data

--- Controller/User/CreateNewUser.js
+++ Controller/User/CreateNewUser.js
@@ -1,12 +1,18 @@
 import DB from "../../ConnectDB/DB.js";
-import { CreateLinks } from "../Navbar/CreateLinks.js";
+import { DefaultSkills } from "../../Constant/Skills.js";
 
 export const CreateNewUser = async (req, res) => {
   try {
-    const Query = "INSERT INTO user (Name, Bio) VALUES (?, ?)";
+    const Query =
+      "INSERT INTO user (Name, Bio, ResumeLink ,GitHubLink ,LinkedInLink ,TwitterLink,MediumLink) VALUES (?, ? ,? ,? ,? ,? ,? )";
     const Value = [
       "Mohamed Osama",
       "👨‍💻Full Stack Developer with expertise in React.js, React Native, Node.js & MongoDB. Passionate about building performant, scalable apps that solve real business needs.🏆 Experienced in AJAX, React Query, Express, and writing clean, maintainable code.🌱",
+      "https://drive.google.com/file/d/1iUR1S4ONU1R4PDrqrQOlFuzbH6jK9DPE/view?usp=drivesdk",
+      "https://github.com/moshaosama",
+      "https://www.linkedin.com/in/thisfekry/",
+      "https://x.com/mohamedosfekry",
+      "https://medium.com/@mohamedosfekry",
     ];
 
     const [resultCraete] = await DB.promise().query(Query, Value);
@@ -16,6 +22,22 @@ export const CreateNewUser = async (req, res) => {
 
     const [resultGet] = await DB.promise().query(UserQuery, valueUser);
 
+    const QueryCreateProject =
+      "INSERT INTO projects (image, title, description,skills,githubLink,liveDemoLink,user_id) VALUES (?,?,?,?,?,?,?)";
+
+    const ValueCreateProject = [
+      "",
+      "CategYou - YouTube Playlist Organizer",
+      "Transform your YouTube experience with nested folders, smart sorting, and intuitive drag-and-drop organization for your Liked Videos playlist.",
+      JSON.stringify(DefaultSkills),
+      "https://github.com/moshaosama/MyFolio",
+      "https://my-folio-iota-eight.vercel.app/",
+      resultCraete.insertId,
+    ];
+
+    for (let i = 0; i <= 2; i++) {
+      await DB.promise().query(QueryCreateProject, ValueCreateProject);
+    }
     return res.status(200).json({
       statusbar: "success",
       message: "Created Successfully",
