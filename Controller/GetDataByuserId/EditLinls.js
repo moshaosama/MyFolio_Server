a22eadbe9708cb68f8cb4cddb4e6ccaaba34234b
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 03:33:16 +0300

    fix: correct logic in EditLinls controller

---
+++ Controller/GetDataByuserId/EditLinls.js
@@ -0,0 +1,35 @@
+import DB from "../../ConnectDB/DB.js";
+import { GetUserId } from "../User/GetUserByid.js";
+
+export const EdtiLinks = async (req, res) => {
+  try {
+    const Link = req.body;
+    const { userId } = req.params;
+
+    const LinkToUpdate = Object.entries(Link);
+
+    if (LinkToUpdate.length === 0) {
+      return res.status(400).json({ message: "No valid fields provided" });
+    }
+
+    const Query = `UPDATE user SET ${Object.keys(Link)[0]} = ?  WHERE id = ?`;
+    const Value = [Object.values(Link)[0], userId];
+
+    await DB.promise().query(Query, Value);
+
+    const QueryGetUser = "SELECT * FROM user WHERE id = ?";
+    const ValueGetUser = [userId];
+
+    const [result] = await DB.promise().query(QueryGetUser, ValueGetUser);
+
+    return res.status(200).json({
+      statusbar: "success",
+      user: result,
+    });
+
+    GetUserId();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
