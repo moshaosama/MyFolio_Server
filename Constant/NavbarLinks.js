60cc9c88a6d1f2f815aab395457f3c80af282abb
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 03:33:16 +0300

    refactor: update constant links in NavbarLinks

--- Constant/NavbarLinks.js
+++ Constant/NavbarLinks.js
@@ -1,21 +1,26 @@
 export const defaultNavbarLinks = [
   {
     name: "About",
+    to: "#about",
   },
 
   {
     name: "Projects",
+    to: "#projects",
   },
 
   {
     name: "Skills",
+    to: "#skills",
   },
 
   {
     name: "Experience",
+    to: "#experience",
   },
 
   {
     name: "Contact",
+    to: "#contact",
   },
 ];
