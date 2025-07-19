a7153fd91cafa16be83f09eab8ff6bea3da55d15
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 03:33:16 +0300

    refactor: improve routing logic in DataByUserIdRouter

--- Routers/DataByUserId/DataByUserIdRouter.js
+++ Routers/DataByUserId/DataByUserIdRouter.js
@@ -5,22 +5,26 @@ import { UpdateImageUser } from "../../Controller/GetDataByuserId/UpdateImageUse
 import { Edittags } from "../../Controller/GetDataByuserId/EditTags.js";
 import { EditName } from "../../Controller/GetDataByuserId/EditName.js";
 import { EditBio } from "../../Controller/GetDataByuserId/EditBio.js";
+import { EdtiLinks } from "../../Controller/GetDataByuserId/EditLinls.js";
 
 const DataByUserIdRouter = express.Router();
 const EditImageRouter = express.Router();
 const EditTagsRouter = express.Router();
 const EditNameRouter = express.Router();
 const EditBioRouter = express.Router();
+const EditLinksRouter = express.Router();
 
 DataByUserIdRouter.route("/:userId").get(GetLinks).delete(DeleteLink);
 EditImageRouter.route("/:userId").put(UpdateImageUser);
 EditTagsRouter.route("/:userId").put(Edittags);
 EditNameRouter.route("/:userId").put(EditName);
 EditBioRouter.route("/:userId").put(EditBio);
+EditLinksRouter.route("/:userId").put(EdtiLinks);
 export {
   DataByUserIdRouter,
   EditImageRouter,
   EditTagsRouter,
   EditNameRouter,
   EditBioRouter,
+  EditLinksRouter,
 };
