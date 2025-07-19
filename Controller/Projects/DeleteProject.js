a30a1d62ad739df3bdacf92194b7307cc8089955
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 19:21:00 +0300

    feat: add controller for deleting a project

---
+++ Controller/Projects/DeleteProject.js
@@ -0,0 +1,33 @@
+import Joi from "joi";
+import DB from "../../ConnectDB/DB.js";
+
+const ProjectSchema = Joi.object({
+  project_id: Joi.number().required(),
+});
+
+export const DeleteProject = async (req, res) => {
+  const { value, error } = ProjectSchema.validate(req.params);
+  try {
+    if (error) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+
+    const Query = "DELETE FROM projects WHERE id = ? ";
+    const Value = [value.project_id];
+
+    await DB.promise().query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Deleted successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
