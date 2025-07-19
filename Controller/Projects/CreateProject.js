3e08916a00a74698f58eb3f019d40658a99dbe43
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 19:20:59 +0300

    feat: add controller for creating new project

---
+++ Controller/Projects/CreateProject.js
@@ -0,0 +1,40 @@
+import DB from "../../ConnectDB/DB.js";
+import { DefaultSkills } from "../../Constant/Skills.js";
+import { UserSchema } from "../GetDataByuserId/getLinks.js";
+
+export const CreateProject = async (req, res) => {
+  const { value, error } = UserSchema.validate(req.params);
+  try {
+    if (error) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+
+    const QueryCreateProject =
+      "INSERT INTO projects (image, title, description,skills,githubLink,liveDemoLink,user_id) VALUES (?,?,?,?,?,?,?)";
+
+    const ValueCreateProject = [
+      "",
+      "CategYou - YouTube Playlist Organizer",
+      "Transform your YouTube experience with nested folders, smart sorting, and intuitive drag-and-drop organization for your Liked Videos playlist.",
+      JSON.stringify(DefaultSkills),
+      "https://github.com/moshaosama/MyFolio",
+      "https://my-folio-iota-eight.vercel.app/",
+      value.userId,
+    ];
+
+    await DB.promise().query(QueryCreateProject, ValueCreateProject);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Created successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
