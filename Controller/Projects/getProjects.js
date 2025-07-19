f250164590cd3ebc69291302e11de7692513ecb1
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 16:41:27 +0300

    feat: add controller logic for handling project data

---
+++ Controller/Projects/getProjects.js
@@ -0,0 +1,28 @@
+import DB from "../../ConnectDB/DB.js";
+import { UserSchema } from "../GetDataByuserId/getLinks.js";
+
+export const GetProjects = async (req, res) => {
+  const { value, error } = UserSchema.validate(req.params);
+  try {
+    if (error) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+    const Query = "SELECT * FROM projects WHERE user_id = ?";
+    const Values = [value.userId];
+
+    const [result] = await DB.promise().query(Query, Values);
+
+    return res.status(200).json({
+      statusbar: "success",
+      projects: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
