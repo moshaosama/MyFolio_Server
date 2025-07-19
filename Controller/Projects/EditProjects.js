fd3eb4117e9f5098c68fcd46c542df1ec4432326
moshaosama <mohamedOSFekry@gmail.com>
2025-07-19 23:40:02 +0300

    feat: implement project editing logic in backend

---
+++ Controller/Projects/EditProjects.js
@@ -0,0 +1,65 @@
+import Joi from "joi";
+import DB from "../../ConnectDB/DB.js";
+
+const ProjectSchema = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  skills: Joi.array()
+    .items(
+      Joi.object({
+        name: Joi.string().required(),
+      })
+    )
+    .required(),
+  githubLink: Joi.string().required(),
+  liveDemoLink: Joi.string().required(),
+});
+
+export const EditProjects = async (req, res) => {
+  const { value, error } = ProjectSchema.validate(req.body);
+  const { project_id } = req.params;
+
+  if (error || !project_id) {
+    return res.status(400).json({
+      statusbar: "error",
+      message: "Invalid input data or missing project ID",
+    });
+  }
+
+  try {
+    const Query = `
+      UPDATE projects
+      SET title = ?, description = ?, skills = ?, githubLink = ?, liveDemoLink = ?
+      WHERE id = ?
+    `;
+    const { title, description, skills, githubLink, liveDemoLink } = value;
+
+    const Value = [
+      title,
+      description,
+      JSON.stringify(skills),
+      githubLink,
+      liveDemoLink,
+      project_id,
+    ];
+
+    const [result] = await DB.promise().query(Query, Value);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Project not found",
+      });
+    }
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Project updated successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
