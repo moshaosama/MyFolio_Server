caabb0679c1411226441219a742e7eb9d2dee623
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:49:41 +0300

    refactor(skills): update skills router endpoints

--- Routers/Skills/SkillsRouter.js
+++ Routers/Skills/SkillsRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { getSkills } from "../../Controller/Skills/GetSkills.js";
+import { DeleteSkill } from "../../Controller/Skills/DeleteSkill.js";
+import { CreateSkill } from "../../Controller/Skills/CreateSkill.js";
 
 const skillsRouter = express.Router();
 
 skillsRouter.route("/:userId").get(getSkills);
+skillsRouter.route("/:skill_id").delete(DeleteSkill);
+skillsRouter.route("/").post(CreateSkill);
 
 export default skillsRouter;
