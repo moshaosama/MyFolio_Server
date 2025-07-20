996ee88bba518e7ce7d8ce761415203d7e37f30d
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:49:46 +0300

    feat(skills): add delete skill controller

---
+++ Controller/Skills/DeleteSkill.js
@@ -0,0 +1,31 @@
+import Joi from "joi";
+import DB from "../../ConnectDB/DB.js";
+
+const SkillSchema = Joi.object({
+  skill_id: Joi.number().required(),
+});
+
+export const DeleteSkill = async (req, res) => {
+  const { value, error } = SkillSchema.validate(req.params);
+  try {
+    if (error || !value.skill_id) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "data is required",
+      });
+    }
+    const Query = "DELETE FROM skills WHERE id = ?";
+    const Value = [value.skill_id];
+    await DB.promise().query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Deleted Successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
