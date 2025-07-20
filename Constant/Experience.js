fc4ec6e468d9beff9c6ac00941de354ec7b54379
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:49:45 +0300

    feat(constants): add experience constants

---
+++ Constant/Experience.js
@@ -0,0 +1,21 @@
+export const DefaultExperience = [
+  {
+    Date: "October 2021 - October 2025",
+    Title: "Bachelor's degree | Computer Science",
+    Foundation: "El Shorouk Academy",
+    Description:
+      "I studied computer and systems engineering, which provided me with a solid foundation in programming, algorithms, data structures, and software engineering. I also developed a strong understanding of computer architecture, operating systems, and computer networks.",
+    Icon: "Education",
+    Position: "left",
+  },
+
+  {
+    Date: "June 2023 – May 2024",
+    Title: "AI Trainer",
+    Foundation: "Scale AI",
+    Description:
+      "Worked on improving a language model for solving Codeforces problems. For each coding problem, I write the solution of the problem, test cases to confirm that my code works fine, a summary of the problem, and an explanation of how my code solves the problem.",
+    Icon: "Development",
+    Position: "right",
+  },
+];
