0c6ef254168ba7de58454b10f82e6748a310d727
moshaosama <mohamedOSFekry@gmail.com>
2025-07-20 23:49:45 +0300

    feat(contact): add contact controllers

---
+++ Controller/Contact/EditContact.js
@@ -0,0 +1,31 @@
+import DB from "../../ConnectDB/DB.js";
+
+export const EditContact = async (req, res) => {
+  try {
+    const body = req.body;
+
+    if (!body.user_id || !body) {
+      return res.status(404).json({
+        statusbar: "error",
+        error: "data is required",
+      });
+    }
+
+    const Query = `UPDATE contact SET ${
+      Object.keys(body)[0]
+    } = ? WHERE user_id = ?`;
+    const Value = [Object.values(body)[0], body.user_id];
+
+    await DB.promise().query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Contact updated successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
---
+++ Controller/Contact/GetContact.js
@@ -0,0 +1,29 @@
+import DB from "../../ConnectDB/DB.js";
+import { UserSchema } from "../GetDataByuserId/getLinks.js";
+
+export const GetContact = async (req, res) => {
+  const { value, error } = UserSchema.validate(req.params);
+  try {
+    if (error) {
+      return res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+
+    const Query = "SELECT * FROM contact WHERE user_id = ?";
+    const Value = [value.userId];
+
+    const [result] = await DB.promise().query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      skills: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
