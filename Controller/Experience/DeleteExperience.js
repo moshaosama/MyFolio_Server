76a8552a714073df34a4c53028a63fb8e5116c5e
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 14:57:57 +0300

    feat(controller): implement delete experience logic

---
+++ Controller/Experience/DeleteExperience.js
@@ -0,0 +1,28 @@
+import Joi from "joi";
+import DB from "../../ConnectDB/DB.js";
+
+const ExperienceSchema = Joi.object({
+  experience_id: Joi.number().required(),
+});
+export const DeleteExperience = async (req, res) => {
+  const { error, value } = ExperienceSchema.validate(req.params);
+  try {
+    if (error) {
+    }
+
+    const Query = "DELETE FROM experience WHERE id = ?";
+    const Value = [value.experience_id];
+
+    await DB.promise().query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Deleted Successfully!",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
