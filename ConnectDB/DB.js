1c05e161bf11e01a667db45a12d0b8aa904e2625
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 20:27:46 +0300

    Latest Version

--- ConnectDB/DB.js
+++ ConnectDB/DB.js
@@ -2,15 +2,12 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
-const DB = mysql.createPool({
+const DB = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_DATABASE || "myfolio",
   port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 export const connectDB = () => {
