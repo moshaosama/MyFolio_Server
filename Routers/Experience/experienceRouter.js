56d829c6a0d9c3bf3d871ad9283933668ce94c75
moshaosama <mohamedOSFekry@gmail.com>
2025-07-21 14:57:53 +0300

    feat(router): wire up experience routes to controller

--- Routers/Experience/experienceRouter.js
+++ Routers/Experience/experienceRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { GetExperience } from "../../Controller/Experience/GetExperience.js";
 import { CreateExperience } from "../../Controller/Experience/CreateExperience.js";
+import { DeleteExperience } from "../../Controller/Experience/DeleteExperience.js";
 
 const ExperineceRouter = express.Router();
 
 ExperineceRouter.route("/:userId").get(GetExperience).post(CreateExperience);
+ExperineceRouter.route("/:experience_id").delete(DeleteExperience);
 
 export default ExperineceRouter;
