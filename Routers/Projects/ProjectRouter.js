e38921df5409bce50cd14a88bce5379df201a907
moshaosama <mohamedOSFekry@gmail.com>
2025-07-24 16:17:50 +0300

    feat(projects): update ProjectRouter with delete skill project route

--- Routers/Projects/ProjectRouter.js
+++ Routers/Projects/ProjectRouter.js
@@ -4,11 +4,14 @@ import { CreateProject } from "../../Controller/Projects/CreateProject.js";
 import { DeleteProject } from "../../Controller/Projects/DeleteProject.js";
 import { EditProjects } from "../../Controller/Projects/EditProjects.js";
 import { GetSkillProject } from "../../Controller/Projects/getSkillProject.js";
+import { DeleteSkillProject } from "../../Controller/Projects/useDeleteSkillProject.js";
 
 const ProjectRouter = express.Router();
 
 ProjectRouter.route("/:userId").get(GetProjects).post(CreateProject);
 ProjectRouter.route("/:project_id").delete(DeleteProject).put(EditProjects);
-ProjectRouter.route("/skills/:project_id").get(GetSkillProject);
+ProjectRouter.route("/skills/:project_id")
+  .get(GetSkillProject)
+  .delete(DeleteSkillProject);
 
 export default ProjectRouter;
