3b0005e5c7857df555ce65bca208b89927293658
moshaosama <mohamedOSFekry@gmail.com>
2025-07-24 15:07:05 +0300

    feat(projects): update ProjectRouter with new routes

--- Routers/Projects/ProjectRouter.js
+++ Routers/Projects/ProjectRouter.js
@@ -3,10 +3,14 @@ import { GetProjects } from "../../Controller/Projects/getProjects.js";
 import { CreateProject } from "../../Controller/Projects/CreateProject.js";
 import { DeleteProject } from "../../Controller/Projects/DeleteProject.js";
 import { EditProjects } from "../../Controller/Projects/EditProjects.js";
+import { GetSkillProject } from "../../Controller/Projects/getSkillProject.js";
 
 const ProjectRouter = express.Router();
 
 ProjectRouter.route("/:userId").get(GetProjects).post(CreateProject);
-ProjectRouter.route("/:project_id").delete(DeleteProject).put(EditProjects);
+ProjectRouter.route("/:project_id")
+  .delete(DeleteProject)
+  .put(EditProjects)
+  .get(GetSkillProject);
 
 export default ProjectRouter;
