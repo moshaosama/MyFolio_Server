cb424d1241f519321005604728faa4eca5bb315f
moshaosama <mohamedOSFekry@gmail.com>
2025-07-24 14:33:23 +0300

    feat(constants): add Messages constant file

---
+++ Constant/Messages.js
@@ -0,0 +1,11 @@
+export const DefaultMessage = {
+  Name: [
+    { message: "My name is " },
+    { message: "Name is " },
+    { message: "I am " },
+    { message: "I'm " },
+    { message: "is " },
+    { message: "" },
+  ],
+  Condition: [{ phase: "Yes" }, { phase: "No" }],
+};
