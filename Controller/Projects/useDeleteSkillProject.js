aba2919b998070f8e1d1faa2e6c5e8cf527373fd
moshaosama <mohamedOSFekry@gmail.com>
2025-07-24 16:17:50 +0300

    feat(projects): add controller for deleting skill project

---
+++ Controller/Projects/useDeleteSkillProject.js
@@ -0,0 +1,29 @@
+import DB from "../../ConnectDB/DB.js";
+
+export const DeleteSkillProject = async (req, res) => {
+  try {
+    const skillProject_id = req.params;
+
+    if (!skillProject_id) {
+      return res.status(404).json({
+        statusbar: "success",
+        message: "data is required",
+      });
+    }
+
+    const Query = "DELETE FROM skill_project WHERE id = ?";
+    const Value = [skillProject_id];
+
+    await DB.query(Query, Value);
+
+    return res.status(200).json({
+      statusbar: "success",
+      message: "Deleted Successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      statusbar: "error",
+      message: err.message || String(err),
+    });
+  }
+};
